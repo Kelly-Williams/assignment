40d5c93e411e68e33a085b8edda6141626edbcc6
Kelly-Williams <kelly.kimathi@strathmore.edu>
2025-07-09 17:50:35 +0300

    Added Javascript commands to html

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// For Sign Up Page
+if (document.getElementById("signupForm")) {
+  document.getElementById("fullname").value = "New User";
+  document.title = "Sign Up - Ready!";
+}
+
+// For Sign In Page
+if (document.getElementById("signinForm")) {
+  document.getElementById("username").value = "Guest";
+  document.title = "Welcome to Sign In";
+}
+
+// For Contact Page
+if (document.getElementById("contactForm")) {
+  document.getElementById("fullname").value = "Your Name";
+  document.title = "Contact Support";
+}
